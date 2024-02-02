bb6a9d607019970754bdd898b1a0f31f6c646984
Reza <rezadewa990@gmail.com>
2024-02-02 23:03:15 +0700

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/components/Masukan.jsx
    	new file:   src/components/TimeAndLocation.jsx
    	new file:   src/components/TopButtons.jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import TopButtons from './components/TopButtons';
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import Masukan from './components/Masukan';
+import TimeAndLocation from './components/TimeAndLocation';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to to-blue-700 shadow-xl shadow-gray-400'>
+      <TopButtons />
+      <Masukan />
+
+      <TimeAndLocation />
     </div>
   );
 }
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TimeAndLocation() {
+    return (
+        <div>
+            <div className='flex items-center justify-center my-6'>
+                <p className='text-white text-xl font-extralight'>
+                    Friday, 2 February 2024 | Local time : 12.01 PM
+                </p>
+            </div>
+
+            <div className='flex items-center justify-center my-3'>
+                <p className='text-white text-3xl font-medium'>
+                    Jakarta, IDN
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default TimeAndLocation
